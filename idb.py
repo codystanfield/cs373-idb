27aa9329a534be8579d650f9d3a438f6979fbdcd
crakkerjak <chris.timaeus@gmail.com>
2016-03-08 17:45:01 -0600

    Runs and renders index.html for a hello world! yay.

--- idb.py
+++ idb.py
@@ -24,26 +24,14 @@ def connect_db():
 def shutdown_session(exception=None):
   db_session.remove()
 
-# http://flask.pocoo.org/docs/0.10/tutorial/dbcon/
-@app.before_request
-def before_request():
-  g.db = connect_db()
-
-
-@app.teardown_request
-def teardown_request(exception):
-  db = getattr(g, 'db', None)
-  if db is not None:
-    db.close()
-
 
 # example route and function from here:
 # http://flask.pocoo.org/docs/0.10/tutorial/views/
 @app.route('/')
 def show_entries():
-  cur = g.db.execute('select title, text from entries order by id desc')
-  entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-  return render_template('show_entries.html', entries=entries)
+  # cur = g.db.execute('select title, text from entries order by id desc')
+  # entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+  return render_template('index.html')#, entries=entries)
 
 
 if __name__ == '__main__':
