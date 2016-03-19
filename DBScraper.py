ea9c44ad51545de263f2d77603fa53f039ddc43f
codystanfield <cody.stanfield2@gmail.com>
2016-03-19 18:55:28 -0500

    #6: Added usage information, split code into different functions to make it easier to add functionality in the future.

--- DBScraper.py
+++ DBScraper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import json, pickle, requests, time
+import json, pickle, requests, signal, sys, time
 
 
 class Cocktail(object):
@@ -13,8 +13,30 @@ class Cocktail(object):
         result += 'Ingredients: {0}, '.format(self.ingredients)
         result += 'Recipe: {0}'.format(self.recipe)
         return result
+
+
+# TODO: figure out how to align well automatically
+def print_usage():
+    print('usage:\n'
+          '--init\t\tget the initial data from The Cocktail Database\n'
+          '-h or --help\tprint this message')
         
         
+# ------------------------------------------------------------------------------
+# INIT
+# ------------------------------------------------------------------------------
+
+
+def run_init():
+    print('Running init')
+    cocktail_list = get_list_of_cocktails()
+    cocktails = get_all_cocktails(cocktail_list)
+
+    # Output to pickle file
+    with open('cocktails.pkl', 'wb') as output:
+        pickle.dump(cocktails, output, pickle.HIGHEST_PROTOCOL)
+
+
 def get_list_of_cocktails():
     """Get the list of cocktails from The Cocktail DB"""
     
@@ -67,18 +89,37 @@ def get_all_cocktails(cocktail_list):
             # Add the cocktail to the list
             cocktails.append(c)
             numCocktails += 1
-            print(numCocktails)
+            print('{0}: {1}'.format(numCocktails, c.name))
             time.sleep(5)   # Throttle the downloads
             
         return cocktails
     
+
+# ------------------------------------------------------------------------------
+# MAIN
+# ------------------------------------------------------------------------------
+
     
 def main():
-    cocktail_list = get_list_of_cocktails()
-    cocktails = get_all_cocktails(cocktail_list)
+    # Catch ctrl-c
+    signal.signal(signal.SIGINT, lambda _, __: sys.exit(0))
     
-    with open('cocktails.pkl', 'wb') as output:
-        pickle.dump(cocktails, output, pickle.HIGHEST_PROTOCOL)
+    # If no options passed in, run the entire program
+    if len(sys.argv) == 1:
+        run_init()
+
+    # If getting the initial data
+    elif sys.argv[1] == '--init':
+        run_init()
+    
+    # Print usage
+    elif sys.argv[1] == '-h' or sys.argv[1] == '--help':
+        print_usage()
+        
+    # Print usage
+    else:
+        print('Unknown command {0}'.format(sys.argv[1]))
+        print_usage()
         
         
 if __name__ == '__main__':
