b6cb9ddc12c03bb9e46c838f30be74b6043b890e
Sydney Shutter <seshutter@mac.com>
2016-03-20 15:52:44 -0500

    Added the tests.py outline
    Only added unit test headers for the models.py file, will add unit
    tests later for the API once it is finished

---
+++ tests.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+# -------
+# imports
+# -------
+
+from io       import StringIO
+from unittest import main, TestCase
+
+from models import Cocktail, Ingredient
+
+# -----------
+# TestNetflix
+# -----------
+
+class TestIdb (TestCase) :
+
+    # ----------
+    # cocktail__init__
+    # ----------
+
+    def test_cocktail_init_1(self) :
+
+
+    def test_cocktail_init_2 (self) :
+
+
+    def test_cocktail_init_3 (self) :
+
+
+    # ----------
+    # cocktail__repr__
+    # ----------
+
+    def test_cocktail_repr_1(self) :
+
+
+    def test_cocktail_repr_2 (self) :
+
+
+    def test_cocktail_repr_3 (self) :
+
+    # ----------
+    # ingredient__init__
+    # ----------
+
+    def test_ingredient_init_1(self) :
+
+
+    def test_ingredient_init_2 (self) :
+
+
+    def test_ingredient_init_3 (self) :
+
+
+    # ----------
+    # ingredient__repr__
+    # ----------
+
+    def test_ingredient_repr_1(self) :
+
+
+    def test_ingredient_repr_2 (self) :
+
+
+    def test_ingredient_repr_3 (self) :
+
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__" :
+    main()
