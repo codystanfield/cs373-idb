7d27a75d6a6a2ff9779a8b2aa01aef51cdf17f04
Sydney Shutter <seshutter@mac.com>
2016-03-20 16:13:31 -0500

    Added code to each unit test

--- tests.py
+++ tests.py
@@ -20,51 +20,109 @@ class TestIdb (TestCase) :
     # ----------
 
     def test_cocktail_init_1(self) :
-
+        name    = "Vodka Sprite"
+        glass   = "Glass"
+        recipe  = "Vodka Sprite Recipe"
+        cocktail = Cocktail.__init__(self, name, glass, recipe)
+        self.assertEqual(name,  cocktail.name)
+        self.assertEqual(glass,  cocktail.glass)
+        self.assertEqual(recipe,  cocktail.recipe)
+        self.assertEqual(None,  cocktail.image)
 
     def test_cocktail_init_2 (self) :
-
+        name    = "Vodka Sprite"
+        glass   = "Glass"
+        recipe  = "Vodka Sprite Recipe"
+        cocktail = Cocktail.__init__(self, name, glass, recipe)
+        self.assertEqual(name,  cocktail.name)
+        self.assertEqual(glass,  cocktail.glass)
+        self.assertEqual(recipe,  cocktail.recipe)
+        self.assertEqual(None,  cocktail.image)
 
     def test_cocktail_init_3 (self) :
-
+        name    = "Vodka Sprite"
+        glass   = "Glass"
+        recipe  = "Vodka Sprite Recipe"
+        cocktail = Cocktail.__init__(self, name, glass, recipe)
+        self.assertEqual(name,  cocktail.name)
+        self.assertEqual(glass,  cocktail.glass)
+        self.assertEqual(recipe,  cocktail.recipe)
+        self.assertEqual(None,  cocktail.image)
 
     # ----------
     # cocktail__repr__
     # ----------
 
     def test_cocktail_repr_1(self) :
-
+        name    = "Vodka Sprite"
+        glass   = "Glass"
+        recipe  = "Vodka Sprite Recipe"
+        cocktail = Cocktail.__init__(self, name, glass, recipe)
+        drink = cocktail.__repr__(self)
+        self.assertEqual("<Cocktail Vodka Sprite>", drink)
 
     def test_cocktail_repr_2 (self) :
-
+        name    = "Vodka Sprite"
+        glass   = "Glass"
+        recipe  = "Vodka Sprite Recipe"
+        cocktail = Cocktail.__init__(self, name, glass, recipe)
+        drink = cocktail.__repr__(self)
+        self.assertEqual("<Cocktail Vodka Sprite>", drink)
 
     def test_cocktail_repr_3 (self) :
+        name    = "Vodka Sprite"
+        glass   = "Glass"
+        recipe  = "Vodka Sprite Recipe"
+        cocktail = Cocktail.__init__(self, name, glass, recipe)
+        drink = cocktail.__repr__(self)
+        self.assertEqual("<Cocktail Vodka Sprite>", drink)
 
     # ----------
     # ingredient__init__
     # ----------
 
     def test_ingredient_init_1(self) :
-
+        name    = "Vodka Sprite"
+        ingredient = Ingredient.__init__(self, name)
+        self.assertEqual(name,  ingredient.name)
+        self.assertEqual(None,  ingredient.amount)
+        self.assertEqual(None,  ingredient.image)
 
     def test_ingredient_init_2 (self) :
-
+        name    = "Vodka Sprite"
+        ingredient = Ingredient.__init__(self, name)
+        self.assertEqual(name,  ingredient.name)
+        self.assertEqual(None,  ingredient.amount)
+        self.assertEqual(None,  ingredient.image)
 
     def test_ingredient_init_3 (self) :
-
+        name    = "Vodka Sprite"
+        ingredient = Ingredient.__init__(self, name)
+        self.assertEqual(name,  ingredient.name)
+        self.assertEqual(None,  ingredient.amount)
+        self.assertEqual(None,  ingredient.image)
 
     # ----------
     # ingredient__repr__
     # ----------
 
     def test_ingredient_repr_1(self) :
-
+        name    = "Vodka Sprite"
+        ingredient = Ingredient.__init__(self, name)
+        ingredientName = ingredient.__repr__(self)
+        self.assertEqual("<Ingredient Vodka Sprite>", ingredientName)
 
     def test_ingredient_repr_2 (self) :
-
+        name    = "Vodka Sprite"
+        ingredient = Ingredient.__init__(self, name)
+        ingredientName = ingredient.__repr__(self)
+        self.assertEqual("<Ingredient Vodka Sprite>", ingredientName)
 
     def test_ingredient_repr_3 (self) :
-
+        name    = "Vodka Sprite"
+        ingredient = Ingredient.__init__(self, name)
+        ingredientName = ingredient.__repr__(self)
+        self.assertEqual("<Ingredient Vodka Sprite>", ingredientName)
 
 # ----
 # main
