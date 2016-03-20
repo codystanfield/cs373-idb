f234c314e22fe08285e014e4d4b4a56efd752760
Sydney Shutter <seshutter@mac.com>
2016-03-20 16:48:22 -0500

    Bug with unit tests
    Changed how I was calling the init method for Cocktail and Ingredient
    in tests.py and produced another bug with SQLAlchemy

--- tests.py
+++ tests.py
@@ -23,7 +23,7 @@ class TestIdb (TestCase) :
         name    = "Vodka Sprite"
         glass   = "Glass"
         recipe  = "Vodka Sprite Recipe"
-        cocktail = Cocktail.__init__(self, name, glass, recipe)
+        cocktail = Cocktail(name, glass, recipe)
         self.assertEqual(name,  cocktail.name)
         self.assertEqual(glass,  cocktail.glass)
         self.assertEqual(recipe,  cocktail.recipe)
@@ -33,7 +33,7 @@ class TestIdb (TestCase) :
         name    = "Vodka Sprite"
         glass   = "Glass"
         recipe  = "Vodka Sprite Recipe"
-        cocktail = Cocktail.__init__(self, name, glass, recipe)
+        cocktail = Cocktail(self, name, glass, recipe)
         self.assertEqual(name,  cocktail.name)
         self.assertEqual(glass,  cocktail.glass)
         self.assertEqual(recipe,  cocktail.recipe)
@@ -43,7 +43,7 @@ class TestIdb (TestCase) :
         name    = "Vodka Sprite"
         glass   = "Glass"
         recipe  = "Vodka Sprite Recipe"
-        cocktail = Cocktail.__init__(self, name, glass, recipe)
+        cocktail = Cocktail(self, name, glass, recipe)
         self.assertEqual(name,  cocktail.name)
         self.assertEqual(glass,  cocktail.glass)
         self.assertEqual(recipe,  cocktail.recipe)
@@ -57,7 +57,7 @@ class TestIdb (TestCase) :
         name    = "Vodka Sprite"
         glass   = "Glass"
         recipe  = "Vodka Sprite Recipe"
-        cocktail = Cocktail.__init__(self, name, glass, recipe)
+        cocktail = Cocktail(self, name, glass, recipe)
         drink = cocktail.__repr__(self)
         self.assertEqual("<Cocktail Vodka Sprite>", drink)
 
@@ -65,7 +65,7 @@ class TestIdb (TestCase) :
         name    = "Vodka Sprite"
         glass   = "Glass"
         recipe  = "Vodka Sprite Recipe"
-        cocktail = Cocktail.__init__(self, name, glass, recipe)
+        cocktail = Cocktail(self, name, glass, recipe)
         drink = cocktail.__repr__(self)
         self.assertEqual("<Cocktail Vodka Sprite>", drink)
 
@@ -73,7 +73,7 @@ class TestIdb (TestCase) :
         name    = "Vodka Sprite"
         glass   = "Glass"
         recipe  = "Vodka Sprite Recipe"
-        cocktail = Cocktail.__init__(self, name, glass, recipe)
+        cocktail = Cocktail(self, name, glass, recipe)
         drink = cocktail.__repr__(self)
         self.assertEqual("<Cocktail Vodka Sprite>", drink)
 
@@ -83,21 +83,21 @@ class TestIdb (TestCase) :
 
     def test_ingredient_init_1(self) :
         name    = "Vodka Sprite"
-        ingredient = Ingredient.__init__(self, name)
+        ingredient = Ingredient(self, name)
         self.assertEqual(name,  ingredient.name)
         self.assertEqual(None,  ingredient.amount)
         self.assertEqual(None,  ingredient.image)
 
     def test_ingredient_init_2 (self) :
         name    = "Vodka Sprite"
-        ingredient = Ingredient.__init__(self, name)
+        ingredient = Ingredient(self, name)
         self.assertEqual(name,  ingredient.name)
         self.assertEqual(None,  ingredient.amount)
         self.assertEqual(None,  ingredient.image)
 
     def test_ingredient_init_3 (self) :
         name    = "Vodka Sprite"
-        ingredient = Ingredient.__init__(self, name)
+        ingredient = Ingredient(self, name)
         self.assertEqual(name,  ingredient.name)
         self.assertEqual(None,  ingredient.amount)
         self.assertEqual(None,  ingredient.image)
@@ -108,19 +108,19 @@ class TestIdb (TestCase) :
 
     def test_ingredient_repr_1(self) :
         name    = "Vodka Sprite"
-        ingredient = Ingredient.__init__(self, name)
+        ingredient = Ingredient(self, name)
         ingredientName = ingredient.__repr__(self)
         self.assertEqual("<Ingredient Vodka Sprite>", ingredientName)
 
     def test_ingredient_repr_2 (self) :
         name    = "Vodka Sprite"
-        ingredient = Ingredient.__init__(self, name)
+        ingredient = Ingredient(self, name)
         ingredientName = ingredient.__repr__(self)
         self.assertEqual("<Ingredient Vodka Sprite>", ingredientName)
 
     def test_ingredient_repr_3 (self) :
         name    = "Vodka Sprite"
-        ingredient = Ingredient.__init__(self, name)
+        ingredient = Ingredient(self, name)
         ingredientName = ingredient.__repr__(self)
         self.assertEqual("<Ingredient Vodka Sprite>", ingredientName)
 
