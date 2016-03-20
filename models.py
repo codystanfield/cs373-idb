a68b1923051db9b24f5dfbefebd79739f2cb0882
crakkerjak <chris.timaeus@gmail.com>
2016-03-20 15:08:46 -0500

    Corrected errors in models.py

--- models.py
+++ models.py
@@ -1,17 +1,40 @@
 # sample from docs:
 # http://flask.pocoo.org/docs/0.10/patterns/sqlalchemy/
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String, Integer
 from database import Base
 
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    email = Column(String(120), unique=True)
 
-    def __init__(self, name=None, email=None):
+class Cocktail(Base):
+    __tablename__ = 'cocktails'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), primary_key=True)
+    glass = Column(String(50))
+    ingredients = db.relationship('Ingredient', backref='cocktail',
+                                  lazy='dynamic')
+    recipe = Column(String(1024))
+    image = Column(String(128))
+
+    def __init__(self, name, glass, recipe, image=None):
         self.name = name
-        self.email = email
+        self.glass = glass
+        self.recipe = recipe
+        self.image = image
 
     def __repr__(self):
-        return '<User %r>' % (self.name)
+        return '<Cocktail %r>' % (self.name)
+
+
+class Ingredient(Base):
+    __tablename__ = 'ingredients'
+    id_ = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    amount = Column(String(50))
+    image = Column(String(128))
+
+    def __init__(self, name, amount=None, image=None):
+        this.name = name
+        this.amount = amount
+        this.image = image
+
+    def __repr__(self):
+        return '<Ingredient %r>' % (self.name)
