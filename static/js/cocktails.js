57fdea40c6434f07c074957619846353986f6761
drecoldwell <Andrea@wireless-10-147-37-37.public.utexas.edu>
2016-03-21 12:39:18 -0500

    Can now find drinks!! MAJOR KEY

--- static/js/cocktails.js
+++ static/js/cocktails.js
@@ -1,9 +1,11 @@
 angular.module('mixopediaApp.cocktails', ['ngRoute'])
 
-.controller('cocktailsCtrl', [function($scope){
+.controller('cocktailsCtrl', ['$scope', function($scope){
     //$location.path('cocktails.html')
     $scope.drinks = [
       {name: 'drink1'},
       {name: 'drink2'}
     ];
+
+    console.log($scope.drinks);
 }]);
