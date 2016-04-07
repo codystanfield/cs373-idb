04918648de1db12830758e03ed25235ba2fcc23f
Dre Coldwell <andrea_coldwell@sbcglobal.net>
2016-04-06 20:53:04 -0500

    Ingredients displaying correctly on cocktail table
    @brettcox

--- idb.py
+++ idb.py
@@ -24,7 +24,6 @@ from unittest import TextTestRunner, \
 import json
 
 
-import os, json, subprocess
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -48,119 +47,147 @@ def api_cocktail_list():
         results.append({'name': c.name, 'id': c.id_})
 
     return json.dumps(results)
-    
+
 
 @app.route('/api/cocktail/<int:id_>', methods=['GET'])
 def api_cocktail(id_):
     results = []
-    for c in Cocktail.query.filter(Cocktail.id_ == id_):
-        obj = {}
-        obj['id'] = c.id_
-        obj['name'] = c.name
-        obj['recipe'] = c.recipe
-        obj['glass'] = c.glass
-        obj['imageURL'] = c.image
-        
-        ingredients = []
-        
-        for i in Amount.query.filter(Amount.cocktail == id_):
-            ing = Ingredient.query.filter(Ingredient.id_ == i.ingredient).one_or_none()
-            ingredients.append({'name': ing.name, 'quantity': i.amount})
-        obj['ingredients'] = ingredients
-        
-        results.append(obj)
-        
+
+    c = Cocktail.query.filter(Cocktail.id_ == id_).one_or_none()
+    obj = {}
+    obj['id'] = c.id_
+    obj['name'] = c.name
+    obj['recipe'] = c.recipe
+    obj['glass'] = c.glass
+    obj['imageURL'] = '/static/images/' + c.image
+
+    ingredients = []
+
+    for i in Amount.query.filter(Amount.cocktail == id_):
+        ing = Ingredient.query.filter(Ingredient.id_ == i.ingredient).one_or_none()
+        ingredients.append({'name': ing.name, 'quantity': i.amount})
+    obj['ingredients'] = ingredients
+
+    results.append(obj)
+
     return json.dumps(results)
 
 
 @app.route('/api/cocktail/<int:id_>/name', methods=['GET'])
 def api_cocktail_name(id_):
-    return ('', 501)
+    results = []
+    c = Cocktail.query.filter(Cocktail.id_ == id_).one_or_none()
+    return json.dumps({'name': c.name})
 
 
 @app.route('/api/cocktail/<int:id_>/ingredients', methods=['GET'])
 def api_cocktail_ingredients(id_):
-    return ('', 501)
+    results = []
+    c = Cocktail.query.filter(Cocktail.id_ == id_).one_or_none()
+    
+    for i in Amount.query.filter(Amount.cocktail == id_):
+        ing = Ingredient.query.filter(Ingredient.id_ == i.ingredient).one_or_none()
+        results.append({'name': ing.name, 'quantity': i.amount})
+        
+    return json.dumps(results)
 
 
 @app.route('/api/cocktail/<int:id_>/glass', methods=['GET'])
 def api_cocktail_glass(id_):
-    return ('', 501)
+    results = []
+    c = Cocktail.query.filter(Cocktail.id_ == id_).one_or_none()
+    return json.dumps({'glass': c.glass})
 
 
 @app.route('/api/cocktail/<int:id_>/recipe', methods=['GET'])
 def api_cocktail_recipe(id_):
-    return ('', 501)
+    results = []
+    c = Cocktail.query.filter(Cocktail.id_ == id_).one_or_none()
+    return json.dumps({'recipe': c.recipe})
 
 
 @app.route('/api/cocktail/<int:id_>/image', methods=['GET'])
 def api_cocktail_image(id_):
-    return ('', 501)
+    results = []
+    c = Cocktail.query.filter(Cocktail.id_ == id_).one_or_none()
+    return json.dumps({'imageURL': '/static/images/{0}'.format(c.image)})
 
 
 @app.route('/api/ingredient', methods=['GET'])
 def api_ingredient_list():
-    return ('', 501)
+    results = []
+    for i in Ingredient.query.all():
+        results.append({'name': i.name, 'id': i.id_})
+
+    return json.dumps(results)
 
 
 @app.route('/api/ingredient/<int:id_>', methods=['GET'])
 def api_ingredient(id_):
-    return ('', 501)
+    results = []
+    
+    i = Ingredient.query.filter(Ingredient.id_ == id_).one_or_none()
+    obj = {}
+    obj['id'] = i.id_
+    obj['name'] = i.name
+    obj['imageURL'] = i.image
+    
+    cocktails = []
+    
+    for i in Amount.query.filter(Amount.ingredient == id_):
+        c = Cocktail.query.filter(Cocktail.id_ == i.cocktail).one_or_none()
+        cocktails.append(c.id_)
+        
+    obj['cocktails'] = cocktails
+    obj['numberOfCocktails'] = len(cocktails)
+    
+    results.append(obj)
+        
+    return json.dumps(results)
 
 
 @app.route('/api/ingredient/<int:id_>/name', methods=['GET'])
 def api_ingredient_name(id_):
-    return ('', 501)
+    results = []
+    i = Ingredient.query.filter(Ingredient.id_ == id_).one_or_none()
+    return json.dumps({'name': i.name})
 
 
 @app.route('/api/ingredient/<int:id_>/cocktails', methods=['GET'])
 def api_ingredient_cocktails(id_):
-    return ('', 501)
+    results = []
+    i = Ingredient.query.filter(Ingredient.id_ == id_).one_or_none()
+    
+    for a in Amount.query.filter(Amount.ingredient == id_):
+        c = Cocktail.query.filter(Cocktail.id_ == a.cocktail).one_or_none()
+        results.append({'name': c.name, 'id': c.id_})
+        
+    return json.dumps(results)
 
 
 @app.route('/api/ingredient/<int:id_>/image', methods=['GET'])
 def api_ingredient_image(id_):
-    return ('', 501)
+    results = []
+    i = Ingredient.query.filter(Ingredient.id_ == id_).one_or_none()
+    return json.dumps({'imageURL': '/static/images/' + i.image})
 
 
 @app.route('/api/ingredient/<int:id_>/numcocktails', methods=['GET'])
 def api_ingredient_numcocktails(id_):
-    return ('', 501)
+    count = Amount.query.filter(Amount.ingredient == id_).count()
+    return json.dumps({'numCocktails': count})
 
-# @app.route('/tests', methods=['GET'])
-# def tests():
-#     p = subprocess.Popen(["make", "test"],
-#             stdout=subprocess.PIPE,
-#             stderr=subprocess.PIPE,
-#             stdin=subprocess.PIPE)
-#     out, err = p.communicate()
-#     return render_template('tests.html', output=err+out)
 
 @app.route('/tests', methods=['GET'])
-def tests():
+def run_unittests():
     """
     Runs unit tests and returns the result.
     """
-    os.system("make test")
-    f = open("TestOutput.tmp", 'r')
-    result = []
-    for line in f:
-        result += [line]
-    f.close()
-    os.system("make clean")    
-    return render_template('tests.html', result=result);
-
-    # p = subprocess.Popen(["make", "test"],
-    #         stdout=subprocess.PIPE,
-    #         stderr=subprocess.PIPE,
-    #         stdin=subprocess.PIPE)
-    # out, err = p.communicate()
-    # return render_template('tests.html', output=err+out)
-    # io = StringIO()
-    # TextTestRunner(stream=io, verbosity=2).run(makeSuite(TestIdb))
-    # results = io.getvalue().split('\n')
-    # # return results
-    # return render_template("tests.html", text=results)
+    io = StringIO()
+    TextTestRunner(stream=io, verbosity=2).run(makeSuite(TestIdb))
+    results = io.getvalue().split('\n')
+    # return results
+    return render_template("tests.html", text=results)
 
 if __name__ == '__main__':
     manager.run()
