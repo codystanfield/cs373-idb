bf3862ddc01b4e27c086198a15244825e9ad9a69
Brett Cox <brettcoxemail@yahoo.com>
2016-04-20 18:50:39 -0500

    Got a rudimentary age checker on the splash page

--- static/js/home.js
+++ static/js/home.js
@@ -1,5 +1,60 @@
 angular.module('mixopediaApp.home', ['ngRoute'])
 
-.controller('homeCtrl', [function(){
-    this.greeting = "konichiwa";
+.controller('homeCtrl', ['$scope', function($scope){
+    $scope.selectedDay;
+    $scope.selectedMonth;
+    $scope.selectedYear;
+    $scope.age;
+
+    $scope.days = [{
+        id: 1,
+        date: 3
+    }, {
+        id: 2,
+        date: 15
+    }, {
+        id: 3,
+        date: 30
+    }];
+
+    $scope.months = [{
+        id: 1,
+        date: 8
+    }, {
+        id: 2,
+        date: 9
+    }, {
+        id: 3,
+        date: 10
+    }];
+
+    $scope.years = [{
+        id: 1,
+        date: 1960
+    }, {
+        id: 2,
+        date: 1963
+    }, {
+        id: 3,
+        date: 1980
+    }];
+
+    $scope.dateDiff = function (birthMonth, birthDay, birthYear) {
+        var todayDate = new Date(),
+            todayYear = todayDate.getFullYear(),
+            todayMonth = todayDate.getMonth(),
+            todayDay = todayDate.getDate(),
+            age = todayYear - birthYear.date;
+
+        if (todayMonth < birthMonth.date - 1) {
+            age--;
+        }
+
+        if (birthMonth.date - 1 === todayMonth && todayDay < birthDay.date) {
+            age--;
+        }
+
+        return $scope.age = age;
+    };
+
 }]);
