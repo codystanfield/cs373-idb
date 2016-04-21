99e8cc197be3df8d71d54d22bf537ae51e343ccf
Brett Cox <brettcoxemail@yahoo.com>
2016-04-20 19:50:52 -0500

    Now correctly determines if you're 21 or older

--- static/js/home.js
+++ static/js/home.js
@@ -7,16 +7,25 @@ angular.module('mixopediaApp.home', ['ngRoute'])
     $scope.selectedMonth;
     $scope.selectedYear;
     $scope.age;
+    $scope.continue = false;
 
     $scope.days = [
-        {id: 1, date: 1}, {id: 2, date: 2}, {id: 3, date: 3}, {id: 4, date: 4},
-        {id: 5, date: 5}, {id: 6, date: 6}, {id: 7, date: 7}, {id: 8, date: 8},
-        {id: 9, date: 9}, {id: 10, date: 10}, {id: 11, date: 11}, {id: 12, date: 12},
-        {id: 13, date: 13}, {id: 14, date: 14}, {id: 15, date: 15}, {id: 16, date: 16},
-        {id: 17, date: 17}, {id: 18, date: 18}, {id: 19, date: 19}, {id: 20, date: 20},
-        {id: 21, date: 21}, {id: 22, date: 22}, {id: 23, date: 23}, {id: 24, date: 24},
-        {id: 25, date: 25}, {id: 26, date: 26}, {id: 27, date: 27}, {id: 28, date: 28},
-        {id: 29, date: 29}, {id: 30, date: 30}, {id: 31, date: 31}];
+        {id: 1, date: 1}, {id: 2, date: 2},
+        {id: 3, date: 3}, {id: 4, date: 4},
+        {id: 5, date: 5}, {id: 6, date: 6},
+        {id: 7, date: 7}, {id: 8, date: 8},
+        {id: 9, date: 9}, {id: 10, date: 10},
+        {id: 11, date: 11}, {id: 12, date: 12},
+        {id: 13, date: 13}, {id: 14, date: 14},
+        {id: 15, date: 15}, {id: 16, date: 16},
+        {id: 17, date: 17}, {id: 18, date: 18},
+        {id: 19, date: 19}, {id: 20, date: 20},
+        {id: 21, date: 21}, {id: 22, date: 22},
+        {id: 23, date: 23}, {id: 24, date: 24},
+        {id: 25, date: 25}, {id: 26, date: 26},
+        {id: 27, date: 27}, {id: 28, date: 28},
+        {id: 29, date: 29}, {id: 30, date: 30},
+        {id: 31, date: 31}];
 
     $scope.months = [
         {id: 1, date: 1}, {id: 2, date: 2}, {id: 3, date: 3}, {id: 4, date: 4},
@@ -90,6 +99,13 @@ angular.module('mixopediaApp.home', ['ngRoute'])
 
         if (birthMonth.date - 1 === todayMonth && todayDay < birthDay.date) {
             age--;
+        }
+
+        if (age >= 21) {
+            $scope.continue = true;
+        }
+        else {
+            $scope.continue = false;
         }
 
         return $scope.age = age;
