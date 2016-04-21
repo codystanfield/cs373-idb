ea2f78d9e0f70aa276392581cfcd445f251df126
codystanfield <cody.stanfield2@gmail.com>
2016-04-20 20:35:32 -0500

    Added querying

--- inverted_index.py
+++ inverted_index.py
@@ -9,29 +9,29 @@ ingredient_idx = {}
 # Loop through Cocktails
 for c in Cocktail.query.all():
     # Name
-    for word in c.name.split(' '):
-        if word not in cocktail_idx:
-            cocktail_idx[word] = set()
-        cocktail_idx[word].add(c.id_)
+    for word in c.name.split():
+        if word.lower() not in cocktail_idx:
+            cocktail_idx[word.lower()] = set()
+        cocktail_idx[word.lower()].add(c.id_)
     
     # Glass
-    for word in c.glass.split(' '):
-        if word not in cocktail_idx:
-            cocktail_idx[word] = set()
-        cocktail_idx[word].add(c.id_)
+    for word in c.glass.split():
+        if word.lower() not in cocktail_idx:
+            cocktail_idx[word.lower()] = set()
+        cocktail_idx[word.lower()].add(c.id_)
     
     # Recipe
     for word in c.recipe:
-        if word not in cocktail_idx:
-            cocktail_idx[word] = set()
-        cocktail_idx[word].add(c.id_)
+        if word.lower() not in cocktail_idx:
+            cocktail_idx[word.lower()] = set()
+        cocktail_idx[word.lower()].add(c.id_)
     
     for i in Amount.query.filter(Amount.cocktail == c.id_):
         ing = Ingredient.query.filter(Ingredient.id_ == i.ingredient).one_or_none()
-        for word in ing.name.split(' '):
-            if word not in cocktail_idx:
-                cocktail_idx[word] = set()
-            cocktail_idx[word].add(c.id_)
+        for word in ing.name.split():
+            if word.lower() not in cocktail_idx:
+                cocktail_idx[word.lower()] = set()
+            cocktail_idx[word.lower()].add(c.id_)
 
 with open('cocktail_idx.pkl', 'wb') as output:
     pickle.dump(cocktail_idx, output, pickle.HIGHEST_PROTOCOL)
@@ -42,24 +42,24 @@ with open('cocktail_idx.pkl', 'wb') as output:
 
 for i in Ingredient.query.all():
     # Name
-    for word in i.name.split(' '):
-        if word not in ingredient_idx:
-            ingredient_idx[word] = set()
-        ingredient_idx[word].add(i.id_)
+    for word in i.name.split():
+        if word.lower() not in ingredient_idx:
+            ingredient_idx[word.lower()] = set()
+        ingredient_idx[word.lower()].add(i.id_)
     
     # Cocktails
     for a in Amount.query.filter(Amount.ingredient == i.id_):
         c = Cocktail.query.filter(Cocktail.id_ == a.cocktail).one_or_none()
-        for word in c.name.split(' '):
-            if word not in ingredient_idx:
-                ingredient_idx[word] = set()
-            ingredient_idx[word].add(i.id_)
+        for word in c.name.split():
+            if word.lower() not in ingredient_idx:
+                ingredient_idx[word.lower()] = set()
+            ingredient_idx[word.lower()].add(i.id_)
         
     # Number of Cocktails
     num = Amount.query.filter(Amount.ingredient == i.id_).count()
-    if num not in ingredient_idx:
-        ingredient_idx[num] = set()
-    ingredient_idx[num].add(i.id_)
+    if str(num) not in ingredient_idx:
+        ingredient_idx[str(num)] = set()
+    ingredient_idx[str(num)].add(i.id_)
 
 with open('ingredient_idx.pkl', 'wb') as output:
     pickle.dump(ingredient_idx, output, pickle.HIGHEST_PROTOCOL)
