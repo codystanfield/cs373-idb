9efc55f4fb03964768f4d41b40bf6bf75e48c52a
Dre Coldwell <andrea_coldwell@sbcglobal.net>
2016-04-20 21:15:05 -0500

    merged master to dre search

--- static/js/search.js
+++ static/js/search.js
@@ -3,7 +3,7 @@
 angular.module('mixopediaApp.search', ['ngRoute'])
 
 
-.controller('searchCtrl', ['$scope', '$routeParams', '$filter', '$location', '$http', function($scope, $routeParams, $filter, $location, $http){
+.controller('searchCtrl', ['$scope', '$routeParams', '$filter', '$location', '$http', '$sce', function($scope, $routeParams, $filter, $location, $http, $sce){
 
   $scope.search = function(query) {
     $location.path('/search/' + query);
@@ -11,37 +11,84 @@ angular.module('mixopediaApp.search', ['ngRoute'])
 
   $scope.query = $routeParams.query;
 
-  
+  $scope.drinks_and = [];
+  $scope.items_and = [];
 
-  // $http({
-  //   method: 'GET',
-  //   url: '/api/cocktail'
-  // }).then(function successCallback(response) {
-  //   // this callback will be called asynchronously
-  //   // when the response is available
-  //   angular.forEach(response.data, function(key){
-  //     var cur_id = key["id"];
-  //     $http({
-  //       method: 'GET',
-  //       url: '/api/cocktail/' + cur_id
-  //     }).then(function successCallback(response) {
-  //       angular.forEach(response.data, function(drink){
-  //         $scope.drinks.push(drink);
-  //       });
-  //     }, function errorCallback(response) {
-  //       // called asynchronously if an error occurs
-  //       // or server returns response with an error status.
-  //       console.log(response);
-  //     });
-  //   });
-  // }, function errorCallback(response) {
-  //   // called asynchronously if an error occurs
-  //   // or server returns response with an error status.
-  //   console.log(response);
-  // });
+  $scope.drinks_or = [];
+  $scope.items_or = [];
+
+  $http({
+    method: 'GET',
+    url: '/api/query',
+    headers: {
+      "query": $scope.query
+    }
+  }).then(function successCallback(response) {
+    // this callback will be called asynchronously
+    // when the response is available
+    //console.log(response.data);
+    console.log(response.data);
+
+    angular.forEach(response.data, function(key, value){
+      var category = value;
+      console.log(value);
+      angular.forEach(key, function(list, boolean) {
+        console.log(boolean);
+        // iterate through the list of items
+        angular.forEach(list, function(item_id) {
+          console.log(item_id);
+          $http({
+            method: 'GET',
+            url: '/api/ + ' + category + '/' + item_id
+          }).then(function successCallback(response) {
+            angular.forEach(response.data, function(drink){
+              $scope.drinks.push(drink);
+            });
+          }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log(response);
+          });
+        });
+      });
+      console.log(key + " value: " + value);
+
+      console.log(value + " " + key.and);
+      console.log(value + " " + key.or);
+
+      // angular.forEach(key.and, )
+      // console.log(cocktails);
+      //var cur_id = cocktails;
+      // $http({
+      //   method: 'GET',
+      //   url: '/api/cocktail/' + cur_id
+      // }).then(function successCallback(response) {
+      //   angular.forEach(response.data, function(drink){
+      //     $scope.drinks.push(drink);
+      //   });
+      // }, function errorCallback(response) {
+      //   // called asynchronously if an error occurs
+      //   // or server returns response with an error status.
+      //   console.log(response);
+      // });
+    });
+  }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+    console.log(response);
+  });
   //
   // $scope.goToCocktail = function(cur_id){
   //   $location.path('/cocktails/' + cur_id.cocktail);
   // };
+
+  $scope.highlight = function(haystack, needle) {
+    if(!needle) {
+      return $sce.trustAsHtml(haystack);
+    }
+    return $sce.trustAsHtml(haystack.replace(new RegExp(needle, "gi"), function(match) {
+      return '<span class="highlightedText">' + match + '</span>';
+    }));
+  };
 
 }]);
